a47d5a1408785e294310075f7939f84f6d64a3ee
helerir <heleri.roosnurm@gmail.com>
2017-05-17 14:59:09 +0300

    working solution of guessing the country without flags

--- Lipud/js/script.js
+++ Lipud/js/script.js
@@ -1,35 +1,35 @@
 	
-	var country = ["NIGERIA", "MOROCCO", "BOTSWANA", "ESTONIA", "FINLAND", "SWEDEN", 
+	var countries = ["NIGERIA", "MOROCCO", "BOTSWANA", "ESTONIA", "FINLAND", "SWEDEN", 
 	"NETHERLANDS", "GERMANY", "BELGIUM"];
 
-	var randomCountry = country[Math.floor(Math.random()*country.length)];
+	var randomCountry = Math.floor(Math.random()*countries.length);
 
 
 	/* stores the country name */
- 	var temp = country[randomCountry];
+ 	var temp = countries[randomCountry];
 	/* declaring and initializing tries */
  	var tries = 0;
 
  	/* the main function */
  	function guessIt() {
- 		var guess = document.input.guess.value;
+ 		var guess = document.getElementById("guess1").value;
  		tries++;
  
-   		window.status = "You have " + tries + "tries left!"
+   		window.status = "You have " + tries + "tries left!";
    		switch(tries) {
  			case 1:
-     			document.input.hint.value = 
+     			document.getElementById("hint").value = 
      			"First Hint : The country name starts with " + temp.charAt(0);
      		break;
      		case 2:
-     			document.input.hint.value = 
+     			document.getElementById("hint").value = 
      			"Second Hint : The country name ends with " + temp.charAt(temp.length - 1);
      		break;
  			case 3:
-     			document.input.hint.value = 
+     			document.getElementById("hint").value = 
      			"Last Hint : The country name has " + temp.length + " characters";
      		break;
-     		default: document.input.hint.value = "No hints are available anymore!";
+     		default: document.getElementById("hint").value = "No hints are available anymore!";
      	}
  
    		if(guess.toUpperCase() == temp) { /* if guess equals to temp */
@@ -38,12 +38,22 @@
      		}
       	}	else	{
  			if(tries == 5) {/* game over */
-    			if(window.confirm("Sorry ! Your chances are over. The country was  " + temp + 
+    			if(window.confirm("Sorry ! Your chances are over. The country was  " + temp + "."
     			"\nDo you want to play again?")) {
       				window.location.reload(); /* reloads the page for a new game */
  				}
  			}
    		}
+ 	}
+
+ 	function catchKeyCode() {//calls when the user press the RETURN key
+ 		if(event.keyCode == 13) {
+    		guessIt();
+    	}
+ 	}
+ 
+ 	function showStatus() {
+ 		window.status = "Tries : " + tries;
  	}
 
 

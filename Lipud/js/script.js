112b58de05c0f70de1234972f99c7908b3bfc2e2
helerir <heleri.roosnurm@gmail.com>
2017-05-17 14:09:41 +0300

    new script file and location

--- Lipud/js/script.js
+++ Lipud/js/script.js
@@ -6,13 +6,46 @@
 
 
 	/* stores the country name */
- 	var temp = country[sr];
+ 	var temp = country[randomCountry];
 	/* declaring and initializing tries */
  	var tries = 0;
 
  	/* the main function */
+ 	function guessIt() {
+ 		var guess = document.input.guess.value;
+ 		tries++;
+ 
+   		window.status = "You have " + tries + "tries left!"
+   		switch(tries) {
+ 			case 1:
+     			document.input.hint.value = 
+     			"First Hint : The country name starts with " + temp.charAt(0);
+     		break;
+     		case 2:
+     			document.input.hint.value = 
+     			"Second Hint : The country name ends with " + temp.charAt(temp.length - 1);
+     		break;
+ 			case 3:
+     			document.input.hint.value = 
+     			"Last Hint : The country name has " + temp.length + " characters";
+     		break;
+     		default: document.input.hint.value = "No hints are available anymore!";
+     	}
+ 
+   		if(guess.toUpperCase() == temp) { /* if guess equals to temp */
+     		if(window.confirm("Absolutely Right ! The country was " + temp + "\nDo you want to play again?")) {
+      			window.location.reload();     /* reloads the page for a new game */
+     		}
+      	}	else	{
+ 			if(tries == 5) {/* game over */
+    			if(window.confirm("Sorry ! Your chances are over. The country was  " + temp + 
+    			"\nDo you want to play again?")) {
+      				window.location.reload(); /* reloads the page for a new game */
+ 				}
+ 			}
+   		}
+ 	}
 
- 	
 
 		/* var currentFlag="nigeria";
 		function draw() {

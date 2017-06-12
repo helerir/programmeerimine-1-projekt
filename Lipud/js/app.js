db65be9fd8faf3776cc25156702d512b5f0ec175
Heleri Roosnurm <heleri.roosnurm@gmail.com>
2017-06-12 19:05:58 +0300

    flags.js

--- Lipud/js/app.js
+++ Lipud/js/app.js
@@ -55,54 +55,9 @@ function showStatus() {
 	window.status = "Tries : " + tries;
 }
 
-var currentFlag=temp;
-
-function draw() {
-	var g=document.getElementById("flag-board").getContext("2d");
-	if(currentFlag=="NIGERIA") {
-		g.clearRect(100, 100, 300, 150);
-		g.fillStyle="#008753";
-		g.fillRect(0, 0, 100, 150);
-		g.fillRect(200, 0, 100, 150);
-		g.fillStyle="#ffffff";
-		g.fillRect(100, 0, 100, 150);
-	}
-	if(currentFlag=="BOTSWANA") {
-	 	g.clearRect(100, 100, 250, 150);
-	 	g.fillStyle="#ffffff";
-	 	g.fillRect(25, 0, 250, 150);
-	 	g.fillStyle="#75AADB";
-	 	g.fillRect(25, 0, 250, 50);
-	 	g.fillRect(25, 100, 250, 50);
-	 	g.fillStyle="#000000";
-	 	g.fillRect(25, 55, 250, 40);
-	}
-	if(currentFlag=="MOROCCO") {
-		g.clearRect(100, 100, 300, 150);
-		g.fillStyle="#c1272d";
-		g.fillRect(25, 0, 250, 150);
-		g.beginPath();
-		g.lineWidth=4;
-		g.strokeStyle="#006233";
-		g.moveTo(150, 50);
-		g.lineTo(135, 100);
-		g.lineTo(175, 70);
-		g.lineTo(125, 70);
-		g.lineTo(165, 100);
-		g.lineTo(150, 50);
-		g.lineTo(147, 60);
-		g.stroke();
-	}
-	if(currentFlag=="ESTONIA"){
-		g.clearRect(100, 100, 300, 150);
-		g.fillStyle="#0072CE";
-		g.fillRect(25, 0, 250, 50);
-		g.fillStyle="#000000";
-		g.fillRect(25, 50, 250, 50);
-		g.fillStyle="#ffffff";
-		g.fillRect(25, 100, 250, 50);
-	}
-}
+var currentFlag = countries[randomCountry];
+ 
+new Flag(currentFlag);
 
 function nigeriaFlag(){
 	currentFlag="NIGERIA"; draw();
--- Lipud/js/flags.js
+++ Lipud/js/flags.js
@@ -1,5 +1,48 @@
-class Flags {
-	constructor(g) {
-		
-	}
-} 
+class Flag {
+  constructor (currentFlag) {
+		var g=document.getElementById("flag-board").getContext("2d");
+			if(currentFlag=="NIGERIA") {
+				g.clearRect(100, 100, 300, 150);
+				g.fillStyle="#008753";
+				g.fillRect(0, 0, 100, 150);
+				g.fillRect(200, 0, 100, 150);
+				g.fillStyle="#ffffff";
+				g.fillRect(100, 0, 100, 150);
+			}
+			if(currentFlag=="BOTSWANA") {
+			 	g.clearRect(100, 100, 250, 150);
+			 	g.fillStyle="#ffffff";
+			 	g.fillRect(25, 0, 250, 150);
+			 	g.fillStyle="#75AADB";
+			 	g.fillRect(25, 0, 250, 50);
+			 	g.fillRect(25, 100, 250, 50);
+			 	g.fillStyle="#000000";
+			 	g.fillRect(25, 55, 250, 40);
+			}
+			if(currentFlag=="MOROCCO") {
+				g.clearRect(100, 100, 300, 150);
+				g.fillStyle="#c1272d";
+				g.fillRect(25, 0, 250, 150);
+				g.beginPath();
+				g.lineWidth=4;
+				g.strokeStyle="#006233";
+				g.moveTo(150, 50);
+				g.lineTo(135, 100);
+				g.lineTo(175, 70);
+				g.lineTo(125, 70);
+				g.lineTo(165, 100);
+				g.lineTo(150, 50);
+				g.lineTo(147, 60);
+				g.stroke();
+			}
+			if(currentFlag=="ESTONIA"){
+				g.clearRect(100, 100, 300, 150);
+				g.fillStyle="#0072CE";
+				g.fillRect(25, 0, 250, 50);
+				g.fillStyle="#000000";
+				g.fillRect(25, 50, 250, 50);
+				g.fillStyle="#ffffff";
+				g.fillRect(25, 100, 250, 50);
+			}
+    }
+}

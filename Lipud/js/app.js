062114fe1c9c6b30fc45518598a835b504f95361
Heleri Roosnurm <heleri.roosnurm@gmail.com>
2017-06-12 18:52:29 +0300

    code indentation corrections2

--- Lipud/js/app.js
+++ Lipud/js/app.js
@@ -17,29 +17,29 @@ function guessIt() {
 	window.status = "You have " + tries + "tries left!";
 	switch(tries) {
 	 	case 1:
-	     	document.getElementById("hint").value = 
-	     	"First Hint : The country name starts with " + temp.charAt(0);
-	    break;
-	    case 2:
-	     	document.getElementById("hint").value = 
-	     	"Second Hint : The country name ends with " + temp.charAt(temp.length - 1);
-	    break;
+	    document.getElementById("hint").value = 
+	    "First Hint : The country name starts with " + temp.charAt(0);
+	  break;
+	  case 2:
+	    document.getElementById("hint").value = 
+	    "Second Hint : The country name ends with " + temp.charAt(temp.length - 1);
+	  break;
 	 	case 3:
-	     	document.getElementById("hint").value = 
-	     	"Last Hint : The country name has " + temp.length + " characters";
-	    break;
-	    default: document.getElementById("hint").value = "No hints are available anymore!";
+	    document.getElementById("hint").value = 
+	    "Last Hint : The country name has " + temp.length + " characters";
+	  break;
+	  default: document.getElementById("hint").value = "No hints are available anymore!";
 	}
 	 
 	if(guess.toUpperCase() == temp) { /* if guess equals to temp */
-	    if(window.confirm("Absolutely Right ! The country was " + temp + ".\nDo you want to play again?")) {
-	      	window.location.reload();     /* reloads the page for a new game */
-	     }
+	  if(window.confirm("Absolutely Right ! The country was " + temp + ".\nDo you want to play again?")) {
+	      window.location.reload();     /* reloads the page for a new game */
+	  }
 	} else {
 	 	if(tries == 5) {/* game over */
-		   	if(window.confirm("Sorry ! Your chances are over. The country was  " + temp +
-		    ".\nDo you want to play again?")) {
-		      	window.location.reload(); /* reloads the page for a new game */
+		  if(window.confirm("Sorry ! Your chances are over. The country was  " + temp +
+		  ".\nDo you want to play again?")) {
+		    window.location.reload(); /* reloads the page for a new game */
 		 	}
 		}
 	}
@@ -47,7 +47,7 @@ function guessIt() {
 
 function catchKeyCode() {//calls when the user press the RETURN key
 	if(event.keyCode == 13) {
-	   	guessIt();
+	  guessIt();
 	}
 }
 	 
